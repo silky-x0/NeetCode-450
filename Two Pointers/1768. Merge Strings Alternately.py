c2c2e301c0d915520e5783f003dc2906372521e8
Akhil <10akhil.t@gmail.com>
2025-02-23 20:42:50 +0530

    LC 1768

--- Two Pointers/1768. Merge Strings Alternately.py
+++ Two Pointers/1768. Merge Strings Alternately.py
@@ -16,3 +16,19 @@ def mergeAlternately(self, word1: str, word2: str) -> str:
 # Approach 3: Two Pointers
 
 # Code is self-explanatory ig.
+
+# I found below code snippet from the Solution section of the problem.
+# It is a very neat and clean code, thought if sharing it here.
+
+   def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+        len1 = len(word1)
+        len2 = len(word2)
+        for i in range(max(len1, len2)):
+            if i < len1:
+                res += word1[i]
+            if i < len2:
+                res += word2[i]
+        
+        return res
+
