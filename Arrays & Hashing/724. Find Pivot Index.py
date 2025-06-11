7eca6549293a1d53a86ceab7edac01baa9759454
Akhil <10akhil.t@gmail.com>
2025-06-11 21:53:28 +0530

    LC 724 ALT

--- Arrays & Hashing/724. Find Pivot Index.py
+++ Arrays & Hashing/724. Find Pivot Index.py
@@ -18,3 +18,15 @@ def pivotIndex(self, nums: List[int]) -> int:
             return i
     else:
         return -1
+    
+#Alternate solution
+def pivotIndex(self, nums: List[int]) -> int:
+    total_sum = sum(nums)
+    left_sum = 0
+
+    for i, num in enumerate(nums):
+        if left_sum == (total_sum - left_sum - num):
+            return i
+        left_sum += num
+
+    return -1    
