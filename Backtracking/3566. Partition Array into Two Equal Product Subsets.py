97092ece64d825b69cdd526b042cb71575774e13
Akhil <10akhil.t@gmail.com>
2025-07-17 08:39:40 +0530

    LC 3566 revisit

--- Backtracking/3566. Partition Array into Two Equal Product Subsets.py
+++ Backtracking/3566. Partition Array into Two Equal Product Subsets.py
@@ -48,3 +48,36 @@ class Solution:
 
 # In simple term, lets element 1 put into A or put into B but we cant put same element in both(disjoint set) and if index in equals to length of nums
 # we'll check that if countA and B are greater than zero, and prodA equals to target equals to prod B then we return True
+
+
+nums1 = [3,1,6,8,4]
+target1 = 24   #O/P -> True
+#target = 91   O/P -> False
+
+def partitionArr(nums1, target1):
+    def backtrack(idx, prodA, prodB, countA, countB):
+        if idx == len(nums1):
+            return countA > 0 and countB > 0 and prodA == prodB == target1
+            
+        elem = nums1[idx]
+
+        if backtrack(idx+1, prodA*elem, prodB, countA+1, countB):
+            return True
+        
+        if backtrack(idx+1, prodA, prodB*elem, countA, countB+1):
+            return True
+        return False
+    return backtrack(0,1,1,0,0)
+
+print(partitionArr(nums1, target1))
+
+# Review:-
+# - i didnt handle base case successfully i was supposed to return countA > 0 and countB > 0 and prodA == prodB == target
+# - but i put this in if condition and was returning true but forgot to return false if this wasnt true!
+# - and i also forgot to implement early pruning, if prodA or prodB > target return false
+
+# Points to remember 
+# - make choice either include or exclude, recursively
+# - count elements you've added to both subset A and B
+# - base condn if index exceeds length of array or index == length + 1 check if any
+# - of subsets is not empty and both subset's product are equal and equals to the target return true else false
