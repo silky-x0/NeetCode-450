0223e0790c20cb41816a37b79098378eba897f5c
3mindedscholar <10akhil.t@gmail.com>
2025-08-22 19:34:42 +0530

    LC 316: Added notes

--- Extras/316. Remove Duplicate Letters.py
+++ Extras/316. Remove Duplicate Letters.py
@@ -1,4 +1,5 @@
-# Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+# Given a string s, remove duplicate letters so that every letter appears once and only once.
+# You must make sure your result is the smallest in lexicographical order among all possible results.
 
 
 # Example 1:
@@ -8,7 +9,7 @@
 # Example 2:
 # Input: s = "cbacdcbc"
 # Output: "acdb"
-
+from collections import Counter
 s = "cbacdcbc"
 
 freq = Counter(s)
@@ -25,3 +26,15 @@ for c in s:
 print("".join(stack))
 
 # Notes and Explanation (will add later)
+# Core idea is to use stack and greedy approach
+# we will use frequency map to track how many times a char has appeared
+# we will use seen set to track which char has already been added to stack
+# we will iterate over each char in string 
+# we will decrease its frequency in fmap
+# if char is already in seen we will skip it
+# else we will check if stack is not empty and top of stack is lexicographically greater
+# than current char and frequency of top of stack char is greater than 0
+# freq > 0 means "this character will come again later, so I can pop it now for a better lexicographic order.
+# if all above condition is true we will pop the top of stack and remove it from seen
+# we will repeat above step until all condition is false
+# finally we will add current char to stack and seen
