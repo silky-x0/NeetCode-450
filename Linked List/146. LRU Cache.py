0463418878b1e117c745d8b9318a7e2fb9ec904c
Akhilesh Tiwari <10akhil.t@gmail.com>
2025-12-28 13:08:59 +0530

    LC 146

---
+++ Linked List/146. LRU Cache.py
@@ -0,0 +1,63 @@
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+# Implement the LRUCache class:
+
+#     LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+#     int get(int key) Return the value of the key if the key exists, otherwise return -1.
+#     void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+
+# The functions get and put must each run in O(1) average time complexity.
+
+class node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left = self.right = node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+lru = LRUCache(2)
+lru.put(1, 1)
+lru.put(2, 2)
+print(lru.get(1))
+lru.put(3, 3)
+print(lru.get(2))
+lru.put(4, 4)
+print(lru.get(1))
+print(lru.get(3))
+print(lru.get(4))
