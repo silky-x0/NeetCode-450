e5eb678d80621f300c5cb7f8223cc4c2cc46e540
Akhilesh Tiwari <10akhil.t@gmail.com>
2026-07-01 22:32:24 +0530

    LC 242 revise and better approach

--- Arrays & Hashing/242. Valid Anagram.py
+++ Arrays & Hashing/242. Valid Anagram.py
@@ -4,8 +4,7 @@
 #Time Complexity is O(n) for sorting.
 #Space Complexity is O(1) since no extra space is used.
 
-#There are other ways to do like split nd join or counting each letter but that
-#would be total bs and nonsensical ig.
+#There are other ways to do like split nd join or counting each letter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -13,3 +12,28 @@ class Solution:
             return True
         else:
             return False
+
+
+# new approach to do this: (better method)
+
+def isAna(s, t):
+    if len(s) == len(t):
+        return False
+
+    ch = {}
+
+    for i in range(len(s)):
+        ch[s[i]] = ch.get(s[i], 0) + 1 
+        ch[t[i]] = ch.get(t[i], 0) - 1
+
+    for val in ch.values():  
+        if val != 0:
+            return False
+
+    return True
+
+
+# Code is self explanatory because we're incrementing and decrementing at same time
+# and if character counts are same then the values would be 0.
+                
+                        
